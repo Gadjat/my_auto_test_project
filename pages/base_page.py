f363fb18744eb4a18c87f079c17c11d83d8b40ba
gadjat@mail.ru <Ghjcnjnfr1234>
2020-07-13 22:23:29 +0300

    NoSuchElementException_test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,20 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
